b4488a3901961bee74b205a89f1d6625d9cf70e3
Kolib Software <kolibsoftware@gmail.com>
2024-03-28 02:08:50 -0600

    Simple socket implementations

---
+++ Rooms.Core/Sockets/WebRoomSocket.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Net.Sockets;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+using KolibSoft.Rooms.Core.Protocol;
+
+namespace KolibSoft.Rooms.Core.Sockets
+{
+
+    public class WebRoomSocket : IRoomSocket
+    {
+        private bool disposed;
+
+        public WebSocket Socket { get; }
+
+        public bool IsAlive => Socket.State == WebSocketState.Open;
+
+        public ArraySegment<byte> SendBuffer { get; private set; }
+        public ArraySegment<byte> ReceiveBuffer { get; private set; }
+
+        public async Task SendAsync(RoomMessage message)
+        {
+            message.CopyTo(SendBuffer);
+            await Socket.SendAsync(SendBuffer[0..message.Length], WebSocketMessageType.Binary, true, default);
+        }
+
+        public async Task ReceiveAsync(RoomMessage message)
+        {
+            await Socket.ReceiveAsync(ReceiveBuffer, default);
+            message.CopyFrom(ReceiveBuffer);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing) Socket.Dispose();
+                SendBuffer = default;
+                ReceiveBuffer = default;
+                disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        public WebRoomSocket(WebSocket socket, ArraySegment<byte> sendBuffer, ArraySegment<byte> receiveBuffer)
+        {
+            Socket = socket;
+            SendBuffer = sendBuffer;
+            ReceiveBuffer = receiveBuffer;
+        }
+
+        public WebRoomSocket(WebSocket socket, int sendBuffering = 1024, int receiveBuffering = 1024)
+        {
+            Socket = socket;
+            SendBuffer = new byte[sendBuffering];
+            ReceiveBuffer = new byte[receiveBuffering];
+        }
+
+    }
+
+}
