dbc1bb640fc3b11f000a1a81e8b69c8d4e185021
Kolib Software <kolibsoftware@gmail.com>
2024-05-11 08:51:49 -0600

    Room Stream class added

--- Rooms.Core/Protocol/RoomStream.cs
+++ Rooms.Core/Protocol/RoomStream.cs
@@ -17,10 +17,7 @@ namespace KolibSoft.Rooms.Core.Protocol
                 _position = 0;
                 _length = await ReadAsync(_buffer, token);
                 if (_length == -1)
-                {
-                    _length = 0;
                     return default;
-                }
             }
             var slice = _buffer.AsMemory().Slice(_position, _length - _position);
             return slice;
@@ -32,13 +29,12 @@ namespace KolibSoft.Rooms.Core.Protocol
             do
             {
                 var chunk = await GetChunkAsync(token);
-                if (chunk.Length == 0) break;
+                if (_length == -1) throw new IOException("Room verb broken");
                 var length = RoomVerb.Scan(chunk.Span);
                 await data.WriteAsync(chunk.Slice(0, length));
                 _position += length;
-
             } while (_position == _length);
-            if (data.Length == 0) throw new IOException("Room verb not found");
+            if (data.Length == 0) return default;
             var verb = new RoomVerb(data.ToArray());
             return verb;
         }
@@ -49,12 +45,12 @@ namespace KolibSoft.Rooms.Core.Protocol
             do
             {
                 var chunk = await GetChunkAsync(token);
-                if (chunk.Length == 0) break;
+                if (_length == -1) throw new IOException("Room channel broken");
                 var length = RoomChannel.Scan(chunk.Span);
                 await data.WriteAsync(chunk.Slice(0, length));
                 _position += length;
             } while (_position == _length);
-            if (data.Length == 0) throw new IOException("Room channel not found");
+            if (data.Length == 0) return default;
             var channel = new RoomChannel(data.ToArray());
             return channel;
         }
@@ -65,39 +61,43 @@ namespace KolibSoft.Rooms.Core.Protocol
             do
             {
                 var chunk = await GetChunkAsync(token);
-                if (chunk.Length == 0) break;
+                if (_length == -1) throw new IOException("Room count broken");
                 var length = RoomCount.Scan(chunk.Span);
                 await data.WriteAsync(chunk.Slice(0, length));
                 _position += length;
             } while (_position == _length);
-            if (data.Length == 0) throw new IOException("Room count not found");
+            if (data.Length == 0) return default;
             var count = new RoomCount(data.ToArray());
             return count;
         }
 
         private async Task<RoomContent> ReadContentAsync(int count, CancellationToken token)
         {
+            if (count == 0) return default;
             var data = new byte[count];
             var index = 0;
             while (index < count)
             {
                 var chunk = await GetChunkAsync(token);
-                if (chunk.Length == 0) break;
+                if (_length == -1) throw new IOException("Room content broken");
                 var length = Math.Min(chunk.Length, count - index);
                 chunk.CopyTo(data.AsMemory().Slice(index, length));
                 index += length;
             }
-            if (index != data.Length) throw new IOException("Room content broken");
             return new RoomContent(data);
         }
 
         public async Task ReadMessageAsync(RoomMessage message, CancellationToken token = default)
         {
             var verb = await ReadVerbAsync(token);
+            if (verb.Length == 0) throw new IOException("Room verb not found");
             var channel = await ReadChannelAsync(token);
+            if (channel.Length == 0) throw new IOException("Room channel not found");
             var count = await ReadCountAsync(token);
+            if (count.Length == 0) throw new IOException("Room count not found");
             var length = (int)count;
             var content = await ReadContentAsync(length, token);
+            if (content.Length != length) throw new IOException("Room content not found");
             message.Verb = verb;
             message.Channel = channel;
             message.Content = content;
@@ -111,7 +111,7 @@ namespace KolibSoft.Rooms.Core.Protocol
             while (index < verb.Length)
             {
                 var length = await WriteAsync(verb.Data.Slice(index), token);
-                if (length == -1) return;
+                if (length == -1) throw new IOException("Room verb broken");
                 index += length;
             }
         }
@@ -122,7 +122,7 @@ namespace KolibSoft.Rooms.Core.Protocol
             while (index < channel.Length)
             {
                 var length = await WriteAsync(channel.Data.Slice(index), token);
-                if (length == -1) return;
+                if (length == -1) throw new IOException("Room channel broken");
                 index += length;
             }
         }
@@ -133,7 +133,7 @@ namespace KolibSoft.Rooms.Core.Protocol
             while (index < count.Length)
             {
                 var length = await WriteAsync(count.Data.Slice(index), token);
-                if (length == -1) return;
+                if (length == -1) throw new IOException("Room count broken");
                 index += length;
             }
         }
@@ -144,7 +144,7 @@ namespace KolibSoft.Rooms.Core.Protocol
             while (index < content.Length)
             {
                 var length = await WriteAsync(content.Data.Slice(index), token);
-                if (length == -1) return;
+                if (length == -1) throw new IOException("Room content broken");
                 index += length;
             }
         }
@@ -160,9 +160,11 @@ namespace KolibSoft.Rooms.Core.Protocol
             await WriteContentAsync(message.Content, token);
         }
 
+        protected RoomStream(ArraySegment<byte> buffer) => _buffer = buffer;
+
         private ArraySegment<byte> _buffer;
-        private int _position;
-        private int _length;
+        private int _position = 0;
+        private int _length = 0;
 
     }
 }
