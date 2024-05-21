6d65ca510551fee0381b8084d5593fab09f78e4f
Kolib Software <kolibsoftware@gmail.com>
2024-05-21 14:57:32 -0600

    Room Stream base JS implementation

--- Rooms.Web/wwwroot/src/streams/room_stream.js
+++ Rooms.Web/wwwroot/src/streams/room_stream.js
@@ -145,11 +145,72 @@ class RoomStream {
     }
 
     async writeAsync(chunk) { throw Error("Abstract member"); }
-    async #writeVerbAsync(verb) { }
-    async #writeChannelAsync(channel) { }
-    async #writeCountAsync(count) { }
-    async #writeContentAsync(content) { }
-    async writeMessageAsync(message) { }
+
+    async #writeVerbAsync(verb) {
+        if (!(verb instanceof RoomVerb)) throw new Error("Invalid argument");
+        if (verb.length > this.#options.maxVerbLength) throw new Error("Room verb too large");
+        let index = 0;
+        while (index < verb.length) {
+            let length = await this.writeAsync(RoomDataUtils.slice(verb.data, index));
+            if (length < 1) throw new Error("Room verb broken");
+            index += length;
+        }
+        await this.writeAsync(BLANK);
+    }
+
+    async #writeChannelAsync(channel) {
+        if (!(channel instanceof RoomChannel)) throw new Error("Invalid argument");
+        if (channel.length > this.#options.maxChannelLength) throw new Error("Room channel too large");
+        let index = 0;
+        while (index < channel.length) {
+            let length = await this.writeAsync(RoomDataUtils.slice(channel.data, index));
+            if (length < 1) throw new Error("Room channel broken");
+            index += length;
+        }
+        await this.writeAsync(BLANK);
+    }
+
+    async #writeCountAsync(count) {
+        if (!(count instanceof RoomCount)) throw new Error("Invalid argument");
+        if (count.length > this.#options.maxChannelLength) throw new Error("Room count too large");
+        let index = 0;
+        while (index < count.length) {
+            let length = await this.writeAsync(RoomDataUtils.slice(count.data, index));
+            if (length < 1) throw new Error("Room count broken");
+            index += length;
+        }
+        await this.writeAsync(BLANK);
+    }
+
+    async #writeContentAsync(content) {
+        if (content.length > this.#options.maxContentLength) throw new Error("Room content too large");
+        let count = new RoomCount(content.length);
+        await this.#writeCountAsync(count);
+        let index = 0;
+        while (index < 0) {
+            this.#writeBuffer.set(content);
+            let _count = content.length;
+            let slice = RoomDataUtils.slice(this.#writeBuffer, 0, _count);
+            let _index = 0;
+            while (_index < slice.length) {
+                let length = await this.writeAsync(RoomDataUtils.slice(slice, _index));
+                if (length < 1) throw new Error("Room content broken");
+                _index += length;
+            }
+            index += slice.length;
+        }
+    }
+
+    async writeMessageAsync(message) {
+        if (!(count instanceof RoomMessage)) throw new Error("Invalid argument");
+        if (this.#disposed) throw new Error("RoomStream disposed");
+        let verb = RoomVerb.parse(message.verb);
+        let channel = new RoomVerb(message.channel);
+        let content = message.content;
+        await this.#writeVerbAsync(verb);
+        await this.#writeChannelAsync(channel);
+        await this.#writeContentAsync(content);
+    }
 
     async onDisposeAsync(disposing) {
         if (!this.#disposed) {
